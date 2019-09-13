d3900fac8cc9309dd692cd6259b31e7aaf000254
Michael Davis <michaeld@iastate.edu>
2019-09-13 16:44:16 -0500

    create weight class and perceptron class

---
+++ Perceptron.h
@@ -0,0 +1,11 @@
+#include "Weight.h"
+using namspace std;
+class Perceptron
+{
+public:
+	float value;
+	Weight input[];
+	Weight output[];
+
+	Perceptron (int, int, float);
+};
---
+++ Weight.h
@@ -0,0 +1,10 @@
+#include "Perceptron.h"
+using standard std;
+class Weight
+{
+	float val;
+	Perceptron input;
+	Perceptron output;
+
+	Weight(Perceptron, Perceptron, float);
+};
