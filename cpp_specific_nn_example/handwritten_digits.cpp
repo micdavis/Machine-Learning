5599ad9816673b3e52a5da8cbb19ec9410e60d9b
Michael Davis <michaeld@iastate.edu>
2019-11-12 21:42:39 -0600

    fully implemented backpropagation

--- cpp_specific_nn_example/handwritten_digits.cpp
+++ cpp_specific_nn_example/handwritten_digits.cpp
@@ -40,57 +40,9 @@ int main () {
 			}
 
 			//on to the back prop
-
-			//this is the output layer biases and weights
-			for(int k = 0; k < 10; k++)
-			{
-				for(int l = 0; l < 15; l++)
-				{
-					weightsTwoDelta[l][k] += tweakWeight(hiddenLayer[l], matrixMultiplySum(hiddenLayer, weightsTwo, k) + outputLayerBias[k], expectedOutput[k]);
-				}
-				outputBiasDelta[k] += tweakBias(matrixMultiplySum(hiddenLayer, weightsTwo, k) + outputLayerBias[k], expectedOutput[k]);
-			}
-
-			//getting the expective activation for the hidden layer
-			double expectedHidden = new double[15];
-			for(int k = 0; k < 10; k++)
-			{
-				for(int l = 0; l < 15; l++)
-				{
-					expectedHidden[l] += expectedActiviation(weightsTwo[l][k], matrixMultiplySum(hiddenLayer, weightsTwo, k) + outputLayerBias[k], expectedOutput[k]);
-				}
-			}
-
-			for(int k = 0; k < 15; k++)
-			{
-				expectedHidden[k] = expectedHidden[k] / 10.0;
-			}
-
-			//repeat above for the hidden later biases and weights
-			for(int k = 0; k < 15; k++)
-			{
-				for(int l = 0; l < 784; l++)
-				{
-					weightsOneDelta[l][k] += tweakWeight(inputLayer[l], matrixMultiplySum(inputLayer, weightsOne, k) + hiddenLayerBias[k], expectedHidden[k]);
-				}
-				hiddenLayerBias[k] += tweakBias(matrixMultiplySum(inputLayer, weightsOne, k) + hiddenLayerBias[k], expectedHidden[k]);
-			}
-		}
-
-		//average all the delta biases and weights and apply them to the NN
-		for(int j = 0; j < 784; j++)
-		{
-			for(int k = 0; k < 15; k++)
-			{
-				weightsOneDelta[j][k] = weightsOneDelta[j][k] / 100.0;
-				weightsOne[j][k] += weightsOneDelta[j][k];
-			}
-		}
-
-		for(int j = 0; j < 15; j++)
-		{
-			hiddenBiasDelta[j] = hiddenBiasDelta[j] / 100.0;
-			hiddenLayerBias[j] = hiddenBiasDelta[j];
+			double* deltaCost = backPropLayer(weightsTwo, outputLayerBias, expectedOutput, hiddenLayer);
+			backPropLayer = backPropLayer(weightsOne, hiddenLayerBias, deltaCost, inputLayer);
+			delete[] deltaCost;
 		}
 	}
 }
@@ -112,34 +64,39 @@ double sigmoid(double x)
 	return 1/(1 + pow(M_E, -x))
 }
 
+double* backPropLayer(double weights[][], double biases[], double deltaCost[], double prevActivation[])
+{
+	double deltaNextActivation[weights.len] = new double[weights.len];
+	double deltaBiases[biases.len] = new double[biases.len];
+	double deltaWeights[weights.len][weights[0].len] = new double[weights.len][weights[0].len];
+
+	//setting the deltas
+	for(int i = 0; i < biases.len; i++)
+	{
+		for(int j = 0; j < weights.len; j++)
+		{
+			deltaBiases[i] += sigmoidDerivative(prevActivation[j] * weights[j][i] + biases[i]) * deltaCost[i];
+			deltaWeight[j][i] = prevActivation[j] * sigmoidDerivative(prevActivation[j] * weights[j][i] + biases[i]) * deltaCost[i];
+			deltaNextActivation[j] += weights[j][i] * sigmoidDerivative(prevActivation[j] * weights[j][i] + biases[i]) * deltaCost[i];
+		}
+	}
+
+	//applying the deltas to the neural net
+	for(int i = 0; i < biases.len; i++)
+	{
+		biases[i] += biases[i];
+		for(int j = 0; j < weights.len; j++)
+		{
+			weights[j][i] += deltaWeight[j][i];
+		}
+	}
+
+	//return for next layer of backprop
+	return deltaNextActivation;
+}
 
 //Used for back prop
 double sigmoidDerivative(double x)
 {
 	return pow(M_E, -x)/pow(1 + pow(M_E, -x), 2)
-}
-
-double calcCost(double actual[], double expected[])
-{
-	double cost = 0;
-	for(int i = 0; i < actual.len; i++)
-	{
-		cost += pow(actual - expected, 2);
-	}
-	return cost;
-}
-
-double tweakWeight(double prevActivation, double currentNeuronVal, double expected)
-{
-	return prevActivation * tweakBias(currentNeuronVal, expected);
-}
-
-double tweakBias(double currentNeuronVal, double expected)
-{
-	return sigmoidDerivative(currentNeuronVal) * 2 * (sigmoid(currentNeuronVal) - expected);
-}
-
-double expectedActiviation(double weight, double currentNeuronVal, double expected)
-{
-	return weight * tweakBias(currentNeuronVal, expected);
 }
