633b219cc636bcd9aab8b88d38166c24a5b5a1cf
Michael Davis <michaeld@iastate.edu>
2019-11-14 17:07:52 -0600

    compiled the neural network example

--- cpp_specific_nn_example/handwritten_digits.cpp
+++ cpp_specific_nn_example/handwritten_digits.cpp
@@ -1,57 +1,83 @@
 #include <cmath>
 using namespace std;
 
+double* backPropLayer(double** weights, double* biases, double* deltaCost, double* prevActivation, int weightsLen, int weightsLenTwo);
+double sigmoid(double x);
+double sigmoidDerivative(double x);
+double matrixMultiplySum(double* inputs, double** weights, int indexInLayer, int weightsLen, int weigthLenTwo);
+
+
 int main () {
 
 	//defining layers of specific size
-	double inputLayer[784] = new double[784];
-	double hiddenLayer[15] = new double[15];
-	double outputLayer[10] = new double[10];
+	double* inputLayer = new double[784];
+	double* hiddenLayer = new double[15];
+	double* outputLayer = new double[10];
 
 	//defining biases of specific size
-	double hiddenLayerBias[15] = new double[15];
-	double outputLayerBias[10] = new double[10];
+	double* hiddenLayerBias = new double[15];
+	double* outputLayerBias = new double[10];
 
 	//defining weights of specific size
-	double weightsOne[784][15] = new double [784][15];
-	double weightsTwo[15][10] = new double [15][10];
+	double** weightsOne = new double* [784];
+	for(int i = 0; i < 784; i++)
+	{
+		weightsOne[i] = new double[15];
+	}
+
+	double** weightsTwo = new double* [15];
+	for(int i = 0; i < 15; i++)
+	{
+		weightsTwo[i] = new double[10];
+	}
 
 	//number of learning cycles
 	for(int i = 0; i < 100; i++)
 	{
-		double weightsOneDelta[784][15] = new double [784][15];
-		double hiddenBiasDelta[15] = new double [15];
-		double weightsTwoDelta[15][10] = new double [15][10]; 
-		double outputBiasDelta[10] = new double[10];
+		double** weightsOneDelta = new double* [784];
+		for(int j = 0; j < 784; j++)
+		{
+			weightsOneDelta[j] = new double[15];
+		}
+
+		double* hiddenBiasDelta = new double [15];
+
+		double** weightsTwoDelta = new double* [15]; 
+		for(int j = 0; j < 15; j++)
+		{
+			weightsTwoDelta[j] = new double[10];
+		}
+
+		double* outputBiasDelta = new double[10];
 		//number of test cases ran through
 		for(int j = 0; j < 100; j++)
 		{
 			//set input layers to an input and expected output
-			double expectedOutput = new double[10];
+			double* expectedOutput = new double[10];
 
 			//forward propagation
 			for(int k = 0; k < 15; k++)
 			{
-				hiddenLayer[k] = sigmoid(matrixMultiplySum(inputLayer, weightsOne, k) + hiddenLayerBias[k]);
+				hiddenLayer[k] = sigmoid(matrixMultiplySum(inputLayer, weightsOne, k, 784, 15) + hiddenLayerBias[k]);
 			}
 			for(int k = 0; k < 10; k++)
 			{
-				outputLayer[k] = sigmoid(matrixMultiplySum(hiddenLayer, weightsTwo, k) + outputLayerBias[k]);
+				outputLayer[k] = sigmoid(matrixMultiplySum(hiddenLayer, weightsTwo, k, 15, 10) + outputLayerBias[k]);
 			}
 
 			//on to the back prop
-			double* deltaCost = backPropLayer(weightsTwo, outputLayerBias, expectedOutput, hiddenLayer);
-			backPropLayer = backPropLayer(weightsOne, hiddenLayerBias, deltaCost, inputLayer);
+			double* deltaCost = backPropLayer(weightsTwo, outputLayerBias, expectedOutput, hiddenLayer, 784, 15);
+			backPropLayer(weightsOne, hiddenLayerBias, deltaCost, inputLayer, 15, 10);
 			delete[] deltaCost;
 		}
 	}
 }
 
 
-double matrixMultiplySum(double inputs[], double weights[][], int indexInLayer)
+double matrixMultiplySum(double* inputs, double** weights, int indexInLayer, int weightsLen, int weightsLenTwo)
 {
 	double sum = 0;
-	for(int i = 0; i < weights.len; i++)
+	for(int i = 0; i < weightsLen; i++)
 	{
 		sum += inputs[i] * weights[i][indexInLayer];
 	}
@@ -61,33 +87,37 @@ double matrixMultiplySum(double inputs[], double weights[][], int indexInLayer)
 //Used for forward prop
 double sigmoid(double x)
 {
-	return 1/(1 + pow(M_E, -x))
+	return 1/(1 + pow(M_E, -x));
 }
 
-double* backPropLayer(double weights[][], double biases[], double deltaCost[], double prevActivation[])
+double* backPropLayer(double** weights, double* biases, double* deltaCost, double* prevActivation, int weightsLen, int weightsLenTwo)
 {
-	double deltaNextActivation[weights.len] = new double[weights.len];
-	double deltaBiases[biases.len] = new double[biases.len];
-	double deltaWeights[weights.len][weights[0].len] = new double[weights.len][weights[0].len];
+	double* deltaNextActivation = new double[weightsLen];
+	double* deltaBiases = new double[sizeof biases/sizeof(double)];
+	double** deltaWeights = new double* [weightsLen];
+	for(int i = 0; i < weightsLen; i++)
+	{
+		deltaWeights[i] = new double [weightsLenTwo];
+	}
 
 	//setting the deltas
-	for(int i = 0; i < biases.len; i++)
+	for(int i = 0; i < (sizeof biases/sizeof(double)); i++)
 	{
-		for(int j = 0; j < weights.len; j++)
+		for(int j = 0; j < weightsLen; j++)
 		{
 			deltaBiases[i] += sigmoidDerivative(prevActivation[j] * weights[j][i] + biases[i]) * deltaCost[i];
-			deltaWeight[j][i] = prevActivation[j] * sigmoidDerivative(prevActivation[j] * weights[j][i] + biases[i]) * deltaCost[i];
+			deltaWeights[j][i] = prevActivation[j] * sigmoidDerivative(prevActivation[j] * weights[j][i] + biases[i]) * deltaCost[i];
 			deltaNextActivation[j] += weights[j][i] * sigmoidDerivative(prevActivation[j] * weights[j][i] + biases[i]) * deltaCost[i];
 		}
 	}
 
 	//applying the deltas to the neural net
-	for(int i = 0; i < biases.len; i++)
+	for(int i = 0; i < (sizeof biases/sizeof(double)); i++)
 	{
 		biases[i] += biases[i];
-		for(int j = 0; j < weights.len; j++)
+		for(int j = 0; j < weightsLen; j++)
 		{
-			weights[j][i] += deltaWeight[j][i];
+			weights[j][i] += deltaWeights[j][i];
 		}
 	}
 
@@ -98,5 +128,5 @@ double* backPropLayer(double weights[][], double biases[], double deltaCost[], d
 //Used for back prop
 double sigmoidDerivative(double x)
 {
-	return pow(M_E, -x)/pow(1 + pow(M_E, -x), 2)
+	return pow(M_E, -x)/pow(1 + pow(M_E, -x), 2);
 }
